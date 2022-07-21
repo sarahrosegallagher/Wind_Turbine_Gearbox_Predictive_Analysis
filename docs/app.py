77dc5745e944983262344c0cde97c27af37b7df3
Isaac Brieske <ipbrieske@gmail.com>
2022-07-21 15:03:18 -0700

    reattached all files in file structure amenable to Flask, all links working

--- docs/app.py
+++ docs/app.py
@@ -1,26 +1,37 @@
 from flask import Flask, render_template, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-import config import rds_password
+#from flask_sqlalchemy import SQLAlchemy
+from config import rds_password
 # import visualizations
 
 app = Flask(__name__)
 
 # Use our RDS database to connect to data
-app.config["MONGO_URI"] = f"postgresql://postgres:{rds_password}@wind-turbine-analysis.chv2nnusygyy.us-west-1.rds.amazonaws.com:5432/wind_turbine_analysis"
-mongo = PyMongo(app)
+#app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://postgres:{rds_password}@wind-turbine-analysis.chv2nnusygyy.us-west-1.rds.amazonaws.com:5432/wind_turbine_analysis"
 
 @app.route("/")
 def index():
-    mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars=mars)
+    return render_template("index.html")
 
 
-@app.route("/scrape")
-def scrape():
-    mars = mongo.db.mars
-    mars_data = scraping.scrape_all()
-    mars.update_one({}, {"$set": mars_data}, upsert=True)
-    return redirect('/', code=302)
+@app.route("/reason_for_analysis.html")
+def reason_for_analysis():
+    return render_template('reason_for_analysis.html')
+
+@app.route("/third.html")
+def third():
+    return render_template('third.html')
+
+@app.route("/fourth.html")
+def fourth():
+    return render_template('fourth.html')
+
+@app.route("/fifth.html")
+def fifth():
+    return render_template('fifth.html')
+
+@app.route("/sixth.html")
+def sixth():
+    return render_template('sixth.html')
 
 if __name__ == '__main__':
     app.run()
