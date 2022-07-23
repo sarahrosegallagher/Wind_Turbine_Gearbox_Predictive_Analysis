15a35dc26c128f6d71067ffe745970247ade8cdf
Isaac Brieske <ipbrieske@gmail.com>
2022-07-23 01:16:25 -0700

    created new branch to flesh out other pages and touch up graphs. Visualization takes 2 min to load, and the graph is wonky, just needs reformatting in buildChart() and optionChanged() needs to be built.

--- docs/static/js/charts.js
+++ docs/static/js/charts.js
@@ -1,17 +1,17 @@
-const graphTypes = []
+const jsonFilePath = "..\\static\\json\\declare.json"
+const graphTypes = ["box", "histogram"]
 
 function init() {
-  var jsonFilePath = "..\\static\\json\\declare.json"
-  
+
   d3.json(jsonFilePath).then((data) => {
     //console.log(data);
     // Grab a reference to the dropdown select element
     var turbineSelector = d3.select("#selTurbine");
     var metricSelector = d3.select("#selMetric");
     var graphSelector = d3.select("#selGraph");
-  
-    var turbineNames = Object.keys(data);
 
+    const turbineNames = Object.keys(data);
+  
     turbineNames.forEach((name) => {
       turbineSelector
         .append("option")
@@ -19,7 +19,9 @@ function init() {
         .property("value", name);
     });
 
-    var metricNames = Object.keys(data['T01']);
+    const metricNames = Object.keys(data['T01']);
+    delete metricNames['turbine_id'];
+    delete metricNames['time_stamp'];
 
     metricNames.forEach((name) => {
       metricSelector
@@ -28,12 +30,19 @@ function init() {
         .property("value", name);
     });
 
-    graphType.forEach((type) => {
+    graphTypes.forEach((type) => {
       graphSelector
         .append("option")
         .text(type)
         .property("value", type);
     });
+
+    var initialTurbine = turbineNames[0]; 
+    var initialMetric = metricNames[2]; 
+    var initialGraph = graphTypes[0]; 
+
+    // Initialize the plot with buildChart
+    buildChart(initialTurbine, initialMetric, initialGraph)
   });
 };
 
@@ -42,7 +51,54 @@ init();
 
 function optionChanged(newSelection) {
 
+  // determine which option changed, and build chart with the new information
+  // CODE GOES HERE 
+
   // Fetch new data each time a new option is selected
-  buildChart(newSelection);
+  // buildChart(newSelection);
   
-}
+};
+
+function buildChart(turbine, metric, graph) {
+
+  let turbine_name = turbine;
+  console.log(turbine_name);
+
+  let metric_type = metric; 
+  console.log(metric_type);
+
+  let graph_type = graph;
+  console.log(graph_type);
+  
+  d3.json(jsonFilePath).then((data) => {
+
+    var plotData = [{
+      x: Object.values(data[turbine_name]['time_stamp']),
+      y: Object.values(data[turbine_name][metric_type]),
+      type: graph_type
+    }]; 
+
+    console.log(plotData[0]['x'])
+
+
+    var trace1 = {
+      y: y0,
+      type: 'box'
+    };
+
+    var plotLayout = {
+      title: graph_type, 
+      xaxis: {
+        title: 'time_stamp'
+      }, 
+      yaxis: {
+        title: metric_type
+      }
+    };
+
+    // Use Plotly to plot the chart 
+    Plotly.newPlot('plot', plotData, plotLayout);
+
+  });
+
+};
